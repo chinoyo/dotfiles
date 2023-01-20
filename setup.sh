3fb13e63e6f1719179fa46c96bb0ee76b82c9b4c
Yuki Chino <yukichino@TABISTMAC230001.local>
2023-01-20 17:03:09 +0900

    starship config and git alias setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
 
 # install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+if [ ! -d ~/.nvm/.git ]; then 
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+else 
+    echo "nvm already installed."
+fi
 
 # install starship
-curl -sS https://starship.rs/install.sh | sh
+if [ ! -f /usr/local/bin/starship ]; then
+    curl -sS https://starship.rs/install.sh | sh
+else
+    echo "starship already installed."
+fi
 
-# add symbolic link for .zshrc
+# add symbolic link for zshrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# add symbolic link for git config
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
+# add symbolic link for starship config
+if [ ! -d ~/.config ]; then
+    mkdir ~/.config
+fi
+
+ln -sf ~/dotfiles/starship.toml ~/.config/starship.toml
